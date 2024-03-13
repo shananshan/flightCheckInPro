0412e0ab6b0a394730fa872d20c698898c5baf12
Caoyue020814 <161260497+Caoyue020814@users.noreply.github.com>
2024-03-14 02:53:04 +0800

    Update Flight.java

--- F21AS-FlightCheckInSystem_Improved/src/Flight.java
+++ F21AS-FlightCheckInSystem_Improved/src/Flight.java
@@ -1,6 +1,14 @@
+import java.time.LocalTime;
+
 public class Flight {
     public static float calulatefee(float dimension, float weight) {
         // calculate the extra fee...
         return 0;
     }
+
+	public static LocalTime getFlightTime() {
+		// TODO Auto-generated method stub
+		//航班的停止值机时间
+		return null;
+	}
 }
