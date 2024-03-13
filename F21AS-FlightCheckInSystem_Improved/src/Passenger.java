4f6a3ae410657036098c64f6dc847c5ca0ea25a0
Caoyue020814 <161260497+Caoyue020814@users.noreply.github.com>
2024-03-14 02:53:23 +0800

    Update Passenger.java

--- F21AS-FlightCheckInSystem_Improved/src/Passenger.java
+++ F21AS-FlightCheckInSystem_Improved/src/Passenger.java
@@ -1,4 +1,12 @@
 public class Passenger {
+	
+	String firstName;
+	String flightCode;
+	Float luggageSize;
+	Float luggageWeight;
+	Boolean checkInSuccess;
+	Boolean feePayment;
+	
     public boolean state() {
         // store the state of the passenger
         return false;
@@ -13,4 +21,31 @@ public class Passenger {
         // give a weight of luggage for each passenger randomly
         return 0;
     }
+
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getFlightCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Float getLuggageWeight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean getCheckInSuccess() {
+		// TODO Auto-generated method stub
+		//初始false
+		return null;
+	}
+
+	public Boolean getFeePayment() {
+		// TODO Auto-generated method stub
+		//初始false
+		return null;
+	}
 }
