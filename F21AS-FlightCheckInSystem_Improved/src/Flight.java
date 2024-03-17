6f6366d2a16a44c128d8c376055065383b6921fd
Yukiyt778 <135519587+Yukiyt778@users.noreply.github.com>
2024-03-17 10:55:10 +0000

    Update Flight.java
    add attributes of passengers and add calculateFee method

--- F21AS-FlightCheckInSystem_Improved/src/Flight.java
+++ F21AS-FlightCheckInSystem_Improved/src/Flight.java
@@ -1,8 +1,43 @@
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Flight {
-    public static float calulatefee(float dimension, float weight) {
+    // Attributes to store flight details.
+    String flightNum; // Unique flight number.
+    int numOfPassengers; // Current number of passengers on the flight.
+    static int maximumPassengers; // Maximum number of passengers 
+    double maximumBaggageWeight; // Maximum total weight of baggage 
+    double maxbaggageVolume; // Maximum volume of baggage allowed per passenger.
+    double extraVolumeFee; // Fee for each unit volume of baggage exceeding the maximum allowed.
+    double extraWeightFee; // Fee for each unit weight of baggage exceeding the maximum allowed.
+
+    static double maxFlightVolume; // Maximum total volume of baggage allowed on the flight.
+    static double maxFlightWeight; // Maximum total weight of baggage allowed on the flight.
+    
+	String flightCode;
+	String destination;
+	String carrier;
+	
+    // List to store passengers on the flight.
+    List<Passenger> passengerList;
+    
+    public Flight(String[] data) {
+		// TODO Auto-generated constructor stub
+        passengerList = new ArrayList<>();
+        flightCode = data[0];
+        destination = data[2];
+        carrier = data[3];
+	}
+
+	public static float calulatefee(float dimension, float weight) {
         // calculate the extra fee...
+		
+        return 0;
+    }
+	
+	public static float calulateload(float dimension, float weight) {
+        // calculate the load percentage...
         return 0;
     }
 
