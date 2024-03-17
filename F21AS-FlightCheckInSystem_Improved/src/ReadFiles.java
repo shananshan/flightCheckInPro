08f9f905b302b311e5f5cbf090c7ad888534c3c0
Yukiyt778 <135519587+Yukiyt778@users.noreply.github.com>
2024-03-17 10:56:29 +0000

    Update ReadFiles.java

--- F21AS-FlightCheckInSystem_Improved/src/ReadFiles.java
+++ F21AS-FlightCheckInSystem_Improved/src/ReadFiles.java
@@ -1,2 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadFiles {
+	//constructor to initialize List of passengers' name and flight code
+	public ReadFiles() {
+		passengerList = new ArrayList<>();
+		flightList = new ArrayList<>();
+	}
+	
+	// declare instance variable as static to make it accessible across all instances of the class
+    static List<Passenger> passengerList;
+    static List<Flight> flightList;
+
+
+    public void readPassengers(String csvFilePath) throws IOException {
+//    	List<Passenger> passenegerName = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(csvFilePath));
+        String line = reader.readLine(); // Assume the first line contains headers and skip it
+        while ((line = reader.readLine())!= null) {
+            String[] data = line.split(",");
+            passengerList.add(new Passenger(data));
+        }
+        reader.close();
+    }
+    
+    public void readFlights(String csvFilePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(csvFilePath));
+        String line = reader.readLine(); // Assume the first line contains headers and skip it
+        while ((line = reader.readLine()) != null) {
+            String[] data = line.split(",");
+            flightList.add(new Flight(data));
+        }
+        reader.close();
+    }
+    
+    public List<Passenger> getPassengerList() {
+        return passengerList;
+    }
+    
+    public List<Flight> getFlightList() {
+        return flightList;
+    }
+    
+   
 }
