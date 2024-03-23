c2bd3c8ff210b948dde3453c0d3680a69f07da45
luxuryLIu <146202585+luxuryLIu@users.noreply.github.com>
2024-03-23 18:00:57 +0000

    Update ReadFiles.java

--- F21AS-FlightCheckInSystem_Improved/src/ReadFiles.java
+++ F21AS-FlightCheckInSystem_Improved/src/ReadFiles.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ReadFiles {
 	//constructor to initialize List of passengers' name and flight code
