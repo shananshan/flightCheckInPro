58604d0548c05f4191eb4df5007c5912c6ff3599
shanshan <576604497@qq.com>
2024-03-23 10:39:48 +0000

    update

--- F21AS-FlightCheckInSystem_Improved/src/CheckInDeskTest.java
+++ F21AS-FlightCheckInSystem_Improved/src/CheckInDeskTest.java
@@ -8,19 +8,20 @@ import static java.lang.Boolean.FALSE;
 
 public class CheckInDeskTest {
     public static void main(String []args) throws IOException {
-        List<Passenger> passengerList = new ArrayList<>();
+        List<Passenger> passengerList;
+        List<Flight> flightList;
         Queue<Passenger> classType0 = new LinkedList<>();
         Queue<Passenger> classType1 = new LinkedList<>();
         ReadFiles fcs = new ReadFiles();
         Queue<Passenger> checked = new LinkedList<>();
-
-
-        fcs.readPassengers("Passenger Bookings2.0.csv");
+        fcs.readPassengers("NEW_passenger_bookings_2.0.csv");
         fcs.readFlights("Flight Detail.csv");
         passengerList = fcs.getPassengerList();
+        flightList = fcs.getFlightList();
         for(Passenger p: fcs.passengerList){
             System.out.println(p);
         }
+
         // Separate passengers into queues based on class type
         CheckInDesk.separatePassengersByClassType(passengerList, classType0, classType1);
 
@@ -30,13 +31,15 @@ public class CheckInDeskTest {
 //            System.out.println(classType0.poll());
 //        }
 //
-//        System.out.println("Class Type 2 Queue:");
-//        while (!classType1.isEmpty()) {
-//            System.out.println(classType1.poll());
-//        }
-        checked = CheckInDesk.genrateEconomyDesk(classType0);
+        System.out.println("Class Type 2 Queue:");
+        while (!classType1.isEmpty()) {
+            System.out.println(classType1.poll());
+        }
+        while (!classType0.isEmpty()){
+            checked = CheckInDesk.generateEconomyDesk(classType0,flightList);
+        }
+//        checked = CheckInDesk.generateEconomyDesk(classType0,flightList);
         System.out.println("Check in ......");
-        System.out.println(checked.poll());
 
 //        System.out.println("Generate a check-in desk successfully.");
     }
--- F21AS-FlightCheckInSystem_Improved/src/MyTimer.java
+++ F21AS-FlightCheckInSystem_Improved/src/MyTimer.java
@@ -1,38 +1,38 @@
 import java.util.*;
 
 public class MyTimer {
-    private static Timer timer = new Timer();
-
-    // passenger enter the queue
-    public static void passengerArrival(List<Passenger> passengerList, Queue<Passenger> economy, Queue<Passenger> business) {
-        // every once in a while, a new passenger enter the queue
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                CheckInDesk.separatePassengersByClassType(passengerList, economy, business);
-
-                System.out.println("New passenger: ");
-            }
-        }, 0, 1000); // 1s
-    }
-
-    public static void flightDeparture(int departureTimeInSeconds, Queue<Passenger> passengerQueue) {
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-//                CheckInDesk.closeDesk();
-//                System.out.println("Flight is departing. Check-in closed.");
-                System.out.println("Processing...");
-                processPassengers(passengerQueue);
-            }
-        }, departureTimeInSeconds * 1000); // * 1000ms
-    }
-
-    private static void processPassengers(Queue<Passenger> passengerQueue) {
-        while (!passengerQueue.isEmpty()) {
-            CheckInDesk.genrateEconomyDesk(passengerQueue);
-            System.out.println("Processing check-in for passenger: " + passengerQueue.poll().getName());
-        }
-    }
+//    private static Timer timer = new Timer();
+//
+//    // passenger enter the queue
+//    public static void passengerArrival(List<Passenger> passengerList, Queue<Passenger> economy, Queue<Passenger> business) {
+//        // every once in a while, a new passenger enter the queue
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                CheckInDesk.separatePassengersByClassType(passengerList, economy, business);
+//
+//                System.out.println("New passenger: ");
+//            }
+//        }, 0, 1000); // 1s
+//    }
+//
+//    public static void flightDeparture(int departureTimeInSeconds, Queue<Passenger> passengerQueue, List<Flight> flightList) {
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+////                CheckInDesk.closeDesk();
+////                System.out.println("Flight is departing. Check-in closed.");
+//                System.out.println("Processing...");
+//                processPassengers(passengerQueue,flightList);
+//            }
+//        }, departureTimeInSeconds * 1000); // * 1000ms
+//    }
+//
+//    private static void processPassengers(Queue<Passenger> passengerQueue,List<Flight> flightList) {
+//        while (!passengerQueue.isEmpty()) {
+//            CheckInDesk.generateEconomyDesk(passengerQueue, flightList);
+//            System.out.println("Processing check-in for passenger: " + passengerQueue.poll().getName());
+//        }
+//    }
 
 }
