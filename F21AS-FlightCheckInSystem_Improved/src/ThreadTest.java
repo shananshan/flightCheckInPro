e5cd5c1284250e9c59aef33858d2743c4601c2c2
shanshan <576604497@qq.com>
2024-03-25 15:10:14 +0000

    update a new test class

---
+++ F21AS-FlightCheckInSystem_Improved/src/ThreadTest.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadTest {
+    public static void main(String[] args) throws IOException {
+
+        List<Passenger> passengerList;
+        List<Flight> flightList;
+        Queue<Passenger> classType0 = new LinkedList<>();
+        Queue<Passenger> classType1 = new LinkedList<>();
+//        Queue<Passenger> economySecurityCheck1 = new LinkedList<>();
+//        Queue<Passenger> economySecurityCheck2 = new LinkedList<>();
+//        Queue<Passenger> economySecurityCheck3 = new LinkedList<>();
+
+        ReadFiles fcs = new ReadFiles();
+        fcs.readPassengers("NEW_passenger_bookings_2.0.csv");
+        fcs.readFlights("Flight Detail.csv");
+        passengerList = fcs.getPassengerList();
+        flightList = fcs.getFlightList();
+
+        // Separate passengers by class type
+        CheckInDesk.separatePassengersByClassType(passengerList, classType0, classType1);
+        System.out.println("Class Type 0 Queue:"+classType0.size());
+        System.out.println("Class Type 1 Queue:"+classType1.size());
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // Create check-in desks
+        executor.execute(new CheckInDesk(classType0,classType1,flightList));
+//        executor.execute(new CheckInDesk(classType0,classType1,flightList));
+//        executor.execute(new CheckInDesk(classType0,classType1,flightList));
+
+        // Create security desks
+//        executor.execute(new );
+
+        // Shutdown the executor
+        executor.shutdown();
+    }
+}
