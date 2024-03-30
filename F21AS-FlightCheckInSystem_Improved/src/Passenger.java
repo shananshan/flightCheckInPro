0e13c1285756912b2c6d305201c01e093ec00632
Yukiyt778 <135519587+Yukiyt778@users.noreply.github.com>
2024-03-30 13:20:02 +0000

    Update Passenger.java

--- F21AS-FlightCheckInSystem_Improved/src/Passenger.java
+++ F21AS-FlightCheckInSystem_Improved/src/Passenger.java
@@ -98,7 +98,7 @@ public class Passenger {
 	}
 
 	public String toString() {
-		return String.format(name + " , " + flightCode + " , " + luggageDimensions + " , " + luggageWeight + " kg");
+		return String.format(name + " , " + flightCode + " , " + luggageSize + " , " + luggageWeight + " , " + classType + " , " + (checkInSuccess ? "True" : "False")+" , "+fee);
 	}
 
 
