0d247a7788c2de1e431a237a295db12c939904c0
luxuryLIu <146202585+luxuryLIu@users.noreply.github.com>
2024-04-01 16:23:42 +0100

    Update Main.java

--- F21AS-FlightCheckInSystem_Improved/src/Main.java
+++ F21AS-FlightCheckInSystem_Improved/src/Main.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,18 +13,31 @@ public class Main{
 	static CheckInDesk checkInDesk1 = null;
 	static CheckInDesk checkInDesk2 = null;
 	static CheckInDesk checkInDesk3 = null;
-	static Queue<Passenger> economySecurityCheck= new LinkedList<>();
-    	static Queue<Passenger> businessSecurityCheck= new LinkedList<>();
+	static Secutiryqueue BSq = null;
+    static Secutiryqueue ESq = null ;
+	
+	static FlightStats stats ;
+    static Map<String, FlightStats> Map ;
+    String eWaitingInfo, bWaitingInfo;
+    static FlightCheckIn flightInfo;
+    static FlightCheckIn FlightHold = new FlightCheckIn();
+	
+    static Queue<Passenger> economySecurityCheck= new LinkedList<>();
+    static Queue<Passenger> businessSecurityCheck= new LinkedList<>();
     
-    	static ConcurrentLinkedQueue<Passenger> businessSecurityQueue = new ConcurrentLinkedQueue<>();
-    	static ConcurrentLinkedQueue<Passenger> economySecurityQueue = new ConcurrentLinkedQueue<>();
+    static ConcurrentLinkedQueue<Passenger> businessSecurityQueue = new ConcurrentLinkedQueue<>();
+    static ConcurrentLinkedQueue<Passenger> economySecurityQueue = new ConcurrentLinkedQueue<>();
 
-
+	
+	
+	
     public static void main(String[] args) throws IOException{
         // we can generate a log by calling a method
-
+       
         List<Passenger> passengerList;
         List<Flight> flightList;
+//        Queue<Passenger> classType0 = new LinkedList<>();
+//        Queue<Passenger> classType1 = new LinkedList<>();
         Queue<Passenger> eClass = new LinkedList<>();
         Queue<Passenger> bClass = new LinkedList<>();
         LocalTime flightTime;
@@ -31,35 +47,48 @@ public class Main{
         fcs.readFlights("Flight Detail.csv");
         passengerList = fcs.getPassengerList();
         flightList = fcs.getFlightList();
-
+        
+        for(Passenger p: fcs.passengerList){
+            System.out.println(p);
+        }
+        
         CheckInDesk.separatePassengersByClassType(passengerList, eClass, bClass);
         checkInDesk1 = new CheckInDesk(1,bClass,eClass,flightList);
         checkInDesk2 = new CheckInDesk(1,bClass,eClass,flightList);
         checkInDesk3 = new CheckInDesk(0,bClass,eClass,flightList);
-
-        View checkInView = new View(checkInDesk1,checkInDesk2,checkInDesk3);
-        Controller checkInController = new Controller(checkInDesk1, checkInDesk2, checkInDesk3, checkInView);
-
-//        // Separate passengers by class type
-        System.out.println("Class Type 0 Queue:"+eClass.size());
-        System.out.println("Class Type 1 Queue:"+bClass.size());
-
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-	economySecurityCheck =CheckInDesk.getEconomyDesk();
+        
+        economySecurityCheck =CheckInDesk.getEconomyDesk();
         businessSecurityCheck = CheckInDesk.getBusinessDesk();
         
-//     ExecutorService executor2 = Executors.newFixedThreadPool(2); // 创建两个线程
+        BSq = new Secutiryqueue(businessSecurityCheck, businessSecurityQueue, "Business");
+        ESq = new Secutiryqueue(economySecurityCheck, economySecurityQueue, "Economy");
+
+        System.out.println("Class Type 0 Queue:"+eClass.size());
+        System.out.println("Class Type 1 Queue:"+bClass.size());
+        
+        ExecutorService executor = Executors.newFixedThreadPool(5);
 
         // Create check-in desks
         executor.execute(checkInDesk1 );
         executor.execute(checkInDesk2 );
         executor.execute(checkInDesk3 );
-	 // 启动安检流程
-        executor.execute(new Secutiryqueue(businessSecurityCheck, businessSecurityQueue, "Business"));
-        executor.execute(new Secutiryqueue(economySecurityCheck, economySecurityQueue, "Economy"));
-
+   
+        
+        View checkInView = new View(checkInDesk1,checkInDesk2,checkInDesk3 ,BSq, ESq);
+        ControllerDesk checkInController = new ControllerDesk(checkInDesk1, checkInDesk2, checkInDesk3, BSq, ESq, checkInView);
+        // 启动安检流程
+        executor.execute(BSq);
+        executor.execute(ESq);
+ 
         executor.shutdown();
-
+        
+       
+    
 
     }
+
+
+
+
+	
 }
