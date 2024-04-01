b2ed37f0fb0ff8d28a8f6ac12bdf4f6a6694e694
luxuryLIu <146202585+luxuryLIu@users.noreply.github.com>
2024-04-01 16:24:26 +0100

    Update Controller.java

--- F21AS-FlightCheckInSystem_Improved/src/Controller.java
+++ F21AS-FlightCheckInSystem_Improved/src/Controller.java
@@ -1,33 +1,26 @@
 
-public class Controller {
+public class ControllerDesk {
 //	 private CheckInDesk checkInDesk;
 	 private View checkInView;
 	 private CheckInDesk checkInDesk1;
 	 private CheckInDesk checkInDesk2;
      private CheckInDesk checkInDesk3;
-	 
-//     public Controller(CheckInDesk checkInDesk1, CheckInDesk checkInDesk2, CheckInDesk checkInDesk3, View checkInView) {
-//         this.checkInDesk1 = checkInDesk1;
-//         this.checkInDesk2 = checkInDesk2;
-//         this.checkInDesk3 = checkInDesk3;
-//         this.checkInView = checkInView;
-//         
-//         // 可以在这里初始化CheckInDesk实例或者监听事件，例如启动值机流程等
-//         // 示例：启动每个CheckInDesk的线程
-////         new Thread(checkInDesk1).start();
-////         new Thread(checkInDesk2).start();
-////         new Thread(checkInDesk3).start();
-//     }
+     private Secutiryqueue BSq;
+     private Secutiryqueue ESq;
 
-	public Controller(CheckInDesk checkInDesk1, CheckInDesk checkInDesk2, CheckInDesk checkInDesk3,View checkInView) {
+
+	public ControllerDesk(CheckInDesk checkInDesk1, CheckInDesk checkInDesk2, CheckInDesk checkInDesk3,
+			Secutiryqueue BSq, Secutiryqueue ESq, View checkInView) {
 		// TODO Auto-generated constructor stub
 		 this.checkInDesk1 = checkInDesk1;
          this.checkInDesk2 = checkInDesk2;
          this.checkInDesk3 = checkInDesk3;
          this.checkInView = checkInView;
-         
-         
+         this.BSq = BSq;
+         this.ESq = ESq;
 	}
+
+}
 
 	
 
