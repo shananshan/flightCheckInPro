f412f41559f43d68efea4523cd9db11177a7db29
luxuryLIu <146202585+luxuryLIu@users.noreply.github.com>
2024-04-03 19:19:55 +0100

    Update View.java

--- F21AS-FlightCheckInSystem_Improved/src/View.java
+++ F21AS-FlightCheckInSystem_Improved/src/View.java
@@ -1,86 +1,3 @@
-//import java.awt.GridLayout;
-//import java.util.Queue;
-//
-//import javax.swing.JFrame;
-//import javax.swing.JLabel;
-//import javax.swing.JPanel;
-//import javax.swing.JScrollPane;
-//import javax.swing.JTextArea;
-//
-//public class View extends JFrame implements Observer{
-//	
-//	private CheckInDesk checkInDesk;
-//	private JTextArea economyQueueTextArea;
-//	private JTextArea businessQueueTextArea;
-//	private JLabel economyLabel;
-//    private JLabel businessLabel;
-//    private JLabel desk1Label;
-//    private JLabel desk2Label;
-//    private JLabel desk3Label;
-//	  
-//	public View(CheckInDesk checkInDesk) {
-//		    super("Check-In Desks Status");
-//	        this.checkInDesk = checkInDesk;
-//	        this.checkInDesk.addObserver(this);   
-//	        desk1Label = new JLabel("Desk 1: ");
-//	        desk2Label = new JLabel("Desk 2: ");
-//	        desk3Label = new JLabel("Desk 3: ");
-//	        this.setLayout(new GridLayout(3, 1)); // 使用网格布局，每个Desk一行
-//	        this.add(desk1Label);
-//	        this.add(desk2Label);
-//	        this.add(desk3Label);
-//	        
-//	        this.setSize(400, 200);
-//	        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//	        this.setVisible(true);
-//	        
-//	        
-//	        
-//	        setLayout(new GridLayout(2, 1)); // 简单布局：两行一列
-//	        economyLabel = new JLabel("Economy Queue Size: 0");
-//	        businessLabel = new JLabel("Business Queue Size: 0");
-//	        
-//	        JPanel panel = new JPanel();
-//	        panel.add(economyLabel);
-//	        panel.add(businessLabel);
-//	
-//	        economyQueueTextArea = new JTextArea();
-//	        businessQueueTextArea = new JTextArea();
-//	
-//	        add(new JScrollPane(economyQueueTextArea)); // 为文本区域添加滚动条
-//	        add(new JScrollPane(businessQueueTextArea));
-//	        add(panel);
-//	
-//	        
-//	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//	        setVisible(true);
-//	    }
-//	
-//
-//	@Override
-//	public void update() {
-//		// TODO Auto-generated method stub
-//		System.out.println("-------------------------------------------------");
-//		 // 假设有方法将队列转换为字符串表示
-//		String economyQueueInfo = queueToString(CheckInDesk.getEconomyDesk());
-//		String businessQueueInfo = queueToString(CheckInDesk.getBusinessDesk());
-//
-//		// 更新GUI组件显示的队列信息
-//		economyQueueTextArea.setText(economyQueueInfo);
-//		businessQueueTextArea.setText(businessQueueInfo);
-//		 System.out.println(economyQueueInfo);
-//		 System.out.println(businessQueueInfo);
-//			}
-//
-//	private String queueToString(Queue<Passenger> queue) {
-//	    StringBuilder sb = new StringBuilder();
-//	    for (Passenger p : queue) {
-//	        sb.append(p.getName()).append(" - ").append(p.getFlightCode()).append("\n");
-//	    }
-//	    return sb.toString();
-//	}
-//}
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -96,8 +13,8 @@ import java.util.Queue;
 public class View extends JFrame implements Observer,ActionListener{
 
     private CheckInDesk checkInDesk1, checkInDesk2, checkInDesk3;
-    private Securityqueue BSq;
-    private Securityqueue ESq;
+    private Securityqueue BSq; //Business class security screening
+    private Securityqueue ESq; //Economy class security screening
     private JTextArea desk1TextArea, desk2TextArea, desk3TextArea;
     private JTextArea flight1TextArea, flight2TextArea, flight3TextArea;
     private JTextArea  security1TextArea, security2TextArea;
@@ -108,13 +25,7 @@ public class View extends JFrame implements Observer,ActionListener{
     FlightCheckIn flightInfo;
     String flightInfo1, flightInfo2, flightInfo3;
     String desk1Info,desk2Info,desk3Info;
-//    FlightStats stats ;
     Map<String, FlightStats> Map ;
-//    private static List<Observer> observers = new ArrayList<>();
-    
-    
-    
-    // Assume these labels are defined elsewhere in your class
     private JLabel desk1Label, desk2Label, desk3Label;
 	private Timer timer;
 	private JLabel label ;
@@ -128,7 +39,7 @@ public class View extends JFrame implements Observer,ActionListener{
         this.checkInDesk3 = checkInDesk3;
         this.BSq = BSq;
         this.ESq = ESq;
-
+        //Add observer to these models.
         this.checkInDesk1.addObserver(this);
         this.checkInDesk2.addObserver(this);
         this.checkInDesk3.addObserver(this);
@@ -147,8 +58,8 @@ public class View extends JFrame implements Observer,ActionListener{
         label = new JLabel("Welcome to AirPort");
         label.setFont(new Font("Arial", Font.PLAIN, 20));
         mainPanel.add(label);
-        timer = new Timer(20,this); // 设置定时器，每50毫秒触发一次
-        timer.start(); // 启动定时器
+        timer = new Timer(20,this); // Set timer
+        timer.start(); 
         
         JPanel waitingPanel = createWaitingPanel();
         mainPanel.add(waitingPanel);
@@ -163,7 +74,7 @@ public class View extends JFrame implements Observer,ActionListener{
         mainPanel.add(flightPanel);
         
         add(mainPanel);
-        pack(); // 根据组件大小自动调整窗口大小
+        pack(); // Automatic window resizing based on component size
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
@@ -187,7 +98,8 @@ public class View extends JFrame implements Observer,ActionListener{
     }
 
     private JPanel createDeskPanel() {
-    	JPanel countersPanel = new JPanel(new GridLayout(1, 3, 10, 10)); // 三个柜台横向布局
+    	//create check in desks panel 
+    	JPanel countersPanel = new JPanel(new GridLayout(1, 3, 10, 10)); 
 
         desk1TextArea = new JTextArea(6, 25);
         desk2TextArea = new JTextArea(6, 25);
@@ -207,8 +119,7 @@ public class View extends JFrame implements Observer,ActionListener{
     	
     }
     private JPanel createFlightStatusPanel() {
-        // Add desk info here
-       
+        // create flight status panel here
           JPanel panel = new JPanel(new GridLayout(1, 3, 10, 10)); // 三个柜台横向布局
           flight1TextArea = new JTextArea(6, 25);
           flight2TextArea = new JTextArea(6, 25);
@@ -228,16 +139,13 @@ public class View extends JFrame implements Observer,ActionListener{
       }
     
     private JPanel createSecurityDeskPanel() {
-        // Add desk info here
-       
-          JPanel panel = new JPanel(new GridLayout(1, 2, 10, 10)); // 三个柜台横向布局
+       //create security desk panels here.
+          JPanel panel = new JPanel(new GridLayout(1, 2, 10, 10)); 
           security1TextArea = new JTextArea(6, 25);
           security2TextArea= new JTextArea(6, 25);
           
-          
           JScrollPane scrollPane1 = new JScrollPane(security1TextArea);
           JScrollPane scrollPane2 = new JScrollPane(security2TextArea);
-          
           
           panel.add(scrollPane1);
           panel.add(scrollPane2);
@@ -250,7 +158,7 @@ public class View extends JFrame implements Observer,ActionListener{
 
     @Override
     public void update() {
-        // 更新Desk状态显示逻辑
+        // update each desk info
     	String ewInfo = queueToString(checkInDesk1.getEconomyQueue());
     	String bwInfo = queueToString(checkInDesk3.getBusinessQueue());
     			
@@ -259,8 +167,9 @@ public class View extends JFrame implements Observer,ActionListener{
         bWTextArea.setText("There are currently "+checkInDesk3.getBusinessQueue().size()+" people waiting in the Business Class Priority Lane:\n"+bwInfo);
 
         if(checkInDesk1.getCurrentPassenger() == null) {
-            desk1Info = "Waiting...";
+            desk1Info = "Waiting..."; // if there is not passenger,print waiting..
         }else {
+        	//set current passenger info to deskInfo
             desk1Info = passToString2(checkInDesk1.getCurrentPassenger());
         }
         if(checkInDesk2.getCurrentPassenger() == null) {
@@ -282,18 +191,20 @@ public class View extends JFrame implements Observer,ActionListener{
         desk2TextArea.setText("Economic Desk2:\n"+ desk2Info);
         desk3TextArea.setText("Business Desk:\n" + desk3Info);
         
-        flightInfo = checkInDesk1.getFighthold();
-  
+        flightInfo = checkInDesk1.getFighthold();  
         List<String> flightCodes = Arrays.asList("DA-4562", "PA-5723", "BI-3892");
         Map = flightInfo.getFlightMap();
+        //get flight status here
         for (String flightcode : flightCodes) {
         	FlightStats flightStats = flightInfo.getFlightStats(Map,flightcode); 	
         	if(flightStats == null) {
+        		//if flightStats is null show waiting in the panel
         		flight1TextArea.setText("Economic waiting...");
         		flight2TextArea.setText("Economic waiting...");
         		flight3TextArea.setText("Business waiting...");
         	}else {
         	if(flightcode.equals("DA-4562")) {
+        		//show flightInfo in flightTextArea
         		String a = String.format("Flight %s\n %d checked of 120\n ",flightcode, flightStats.getPassengerCount());	
         		String fs = FlightToString(flightcode,flightStats);
         		flight1TextArea.setText(a + fs);
@@ -310,6 +221,7 @@ public class View extends JFrame implements Observer,ActionListener{
         	}
         }
         if(BSq.getCurrentPassenger() == null ) {
+        	// if BSq is null show waiting...
         	security1TextArea.setText("Business waiting...");
         }else {
         	String securityInfo1 = securityToString(BSq.getCurrentPassenger());
@@ -328,39 +240,41 @@ public class View extends JFrame implements Observer,ActionListener{
     }
     
     public void actionPerformed(ActionEvent e) {
-        // 当定时器触发时，更新标签的位置
+    	 // update the position of the label when the timer is triggered
         xPos++;
         label.setLocation(xPos, 0);
 
-        // 当标签移出窗口边界时，将其移回窗口左侧
+        // move the tab back to the left side of the window when it moves out of the window boundary
         if (xPos >= getWidth()) {
             xPos = -label.getWidth();
         }
     }
 
     private String passToString1(Passenger p) {
+    	// Change the passenger information to a String variable 
         StringBuilder sb = new StringBuilder();
         sb.append(p.getName()).append(" - ").append(p.getFlightCode()).append(" - ").append("Luggage Size: "+p.getLuggageDimensions()).append(" - ").append("Weight:").append(p.getLuggageWeight()).append("kg").append("\n");
         return sb.toString();
     }
 
     private String passToString2(Passenger p) {
+    	// Change the passenger information to a String variable and Show it to CheckinDesk
         StringBuilder sb = new StringBuilder();
-        sb.append(p.getName()).append(" - ").append(p.getFlightCode()).append("\n").append("Luggage Size: "+p.getLuggageDimensions()).append("\n").append("Weight:").append(p.getLuggageWeight()+"KG").append("\n").append("Fee: "+p.getFee());
+        sb.append(p.getName()).append(" - ").append(p.getFlightCode()).append("\n").append("Luggage Size: "+p.getLuggageDimensions()).append("\n").append("Weight:").append(p.getLuggageWeight()+"KG").append("\n").append("Fee: £"+p.getFee()+"");
         return sb.toString();
     }
     
     private String securityToString(Passenger p) {
+    	// Change the passenger information to a String variable and Show it to Securitydesk
         StringBuilder sb = new StringBuilder();
         sb.append(p.getName()).append(" - ").append(p.getFlightCode()).append("\n");
         return sb.toString();
     }
     
-    
-    
     private String FlightToString(String fc, FlightStats fs) {
+    	// Change the passenger information to a String variable and Show it to Securitydesk
         StringBuilder sb = new StringBuilder();
-        sb.append(fs.getSizeHold(fc)+"% of total Volume").append("\n").append(fs.getWeightHold(fc) + "% of total Weight");
+        sb.append("Volume Hold:"+fs.getSizeHold(fc)+"%").append("\n").append("Weight Hold:"+fs.getWeightHold(fc) + "%").append("\n");
         return sb.toString();
     }
     
