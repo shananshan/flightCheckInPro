aade9d368b4533ecab5d3d19bbbe9270d81271de
luxuryLIu <146202585+luxuryLIu@users.noreply.github.com>
2024-04-03 19:30:40 +0100

    Update Observer.java

--- F21AS-FlightCheckInSystem_Improved/src/Observer.java
+++ F21AS-FlightCheckInSystem_Improved/src/Observer.java
@@ -1,5 +1,9 @@
-
 public interface Observer {
+    /**
+     * The update method is called to notify the Observer about a change in the observable subject's state.
+     * Implementations of this method should define how the observer reacts to these notifications.
+     */
     void update();
 }
+
 
